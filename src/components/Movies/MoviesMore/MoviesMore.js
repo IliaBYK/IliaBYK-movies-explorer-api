69f86509a04b01328a246806f886f20fc29f2d04
Ilia Bykov <107132756+IliaBYK@users.noreply.github.com>
2023-03-11 00:29:04 +0300

    Delete src directory

--- src/components/Movies/MoviesMore/MoviesMore.js
+++
@@ -1,9 +0,0 @@
-function MoviesMore ({onClick, text}) {
-  return (
-    <div className="more">
-      <button className="more__btn button" onClick={onClick}>{text}</button>
-    </div>
-  )
-}
-
-export default MoviesMore;
